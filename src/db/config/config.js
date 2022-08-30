13170e163365217337c7cc7d17134f89247cece1
hito-kotaro <tohi.work@gmail.com>
2022-08-31 07:17:26 +0900

    db接続情報をdevとprodで分離

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,15 +1,29 @@
 require('dotenv').config();
 const ssl = process.env.ENV === 'prod' ? { rejectUnauthorized: true } : '';
-config = {
-  dialect: process.env.DB_DIARECT,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  logging: false,
-  dialectOptions: {
-    ssl,
-  },
-};
+
+if (process.env.ENV === 'prod') {
+  config = {
+    dialect: process.env.DB_DIARECT,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    logging: false,
+    dialectOptions: {
+      ssl: { rejectUnauthorized: true },
+    },
+  };
+} else {
+  config = {
+    dialect: process.env.DB_DIARECT,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    logging: false,
+  };
+}
+
 module.exports = config;
