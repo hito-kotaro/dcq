1b0f141b94638f4e090438aae468885e490bac6e
hito-kotaro <tohi.work@gmail.com>
2022-09-03 23:07:34 +0900

    ユーザーのCRUD作成
    todo: 更新処理が未完成

--- src/routes/api/v1/user/index.ts
+++ src/routes/api/v1/user/index.ts
@@ -1,37 +1,15 @@
 import express, { Request, Response } from 'express';
-import { currentDate } from '../../../../utils/getDate';
 import type { userType } from '../../../../types/models';
 
 const models = require('../../../../db/models');
 
 const router = express.Router();
 
-type newUserType = Omit<userType, 'id' | 'created_at' | 'updated_at'>;
-
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
-// user存在チェック emailを受け取って存在すればTしなければFを返す
-const isExistUser = async (email: string) => {
-  const result = await models.User.findAll({ where: { email } });
-  console.log(result > 0);
-  const isExist = result > 0;
-  return isExist;
-};
-
 // fixme: SELECTで取得した時刻がUTCになってしまう。
 // DBにはJSTでINSERTした時刻になっているが、sequerizeで取り出す時に変換されている？
-router.get('/test', async (_req: Request, res: Response) => {
-  const isExist = await isExistUser('1');
-  if (isExist) {
-    // raiseする
-    console.log(isExist);
-    res.send('user already exist');
-  } else {
-    console.log(isExist);
-    res.send('user not exist');
-  }
-});
 
 // GET  http://localhost:3000/api/v1/user/
 router.get('/', async (req: Request, res: Response) => {
@@ -42,21 +20,53 @@ router.get('/', async (req: Request, res: Response) => {
   res.json(users);
 });
 
+// ユーザー作成
 router.post('/create', async (req: Request, res: Response) => {
-  console.log(req.body);
-  const postuser: newUserType = {
-    name: req.body.name,
-    password: req.body.name,
-    point: 0,
-    account_id: req.body.account_id,
-    group_id: 1,
-    role_id: 1,
-  };
+  models.User.findOrCreate({
+    where: { name: req.body.name, account_id: req.body.account_id },
+    defaults: req.body,
+  }).then(([user, created]: any) => {
+    if (created) {
+      res.json({ status: 200, message: `created account ${req.body.nane}` });
+    } else {
+      res.json({
+        status: 422,
+        message: `account ${req.body.name} is already exist`,
+      });
+    }
+  });
+});
 
-  const result = await models.User.create(postuser);
-  res.send(result);
+// ユーザーの削除
+router.delete('/delete/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  models.User.findOne({
+    where: { id },
+  }).then((user: any) => {
+    if (user) {
+      user.destroy();
+      res.json({ status: 200, message: `delete succeeded ${user.name}` });
+    } else {
+      res.json({
+        status: 422,
+        message: 'delete failed. is not exist',
+      });
+    }
+  });
+});
+
+// アカウントの更新
+router.put('/update/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  models.Accounts.findOne({ where: { id } }).then((user: any) => {
+    if (user) {
+      res.json({ status: 200, message: 'ok exist' });
+    } else {
+      res.json({ status: 422, message: 'no exist' });
+    }
+  });
 });
 
 // routerをモジュールとして扱う準備
-module.exports = router;
 export default router;
